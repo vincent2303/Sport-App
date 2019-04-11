daaf05d006e9c909e6171c76b47711cc1cd383d0
vincent2303 <vincent.martin23@gmail.com>
2019-04-11 01:22:02 +0100

    create mutable redux state

--- src/redux/reducers/exerciseReducer.js
+++ src/redux/reducers/exerciseReducer.js
@@ -1,14 +1,26 @@
+import transformIntoMutableState from '../utils/mutbaleState';
 import { PUSH_EXERCISE } from '../actions/types';
 
 const exerciseInitialState = {
   exercises: [],
+  myMap: new Map([
+    [11, 'aa'],
+    ['cc', 'azerty'],
+  ]),
 };
 
-function exerciseReducer(state = exerciseInitialState, { type }) {
+
+// le return du reducer ne doit pas avoir mutate le state:
+// si mon state c'est {a: 11, b: 222 }
+// je peux, avant le return faire ce que je veux mais au moment du return,
+// si j'ai changé qqch, ce qqch doit etre un nvl objet
+
+function exerciseReducer(immutableState = exerciseInitialState, { type }) {
+  const state = transformIntoMutableState(immutableState);
   switch (type) {
     case PUSH_EXERCISE: {
-      const exercises = state.exercises.slice(0);
-      exercises.push('AAAA');
+      const { exercises } = state;
+      exercises.push('aaaa');
       return { ...state, exercises };
     }
     default:
